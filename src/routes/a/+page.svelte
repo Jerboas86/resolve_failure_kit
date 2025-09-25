dca001dd733b810a2eec685d38aaca4ca8a832ea
Jerboas86 <benoitdelemps@gmail.com>
2025-09-25 11:29:03 +0200

    make resolve failed only in v1.43

--- src/routes/a/+page.svelte
+++ src/routes/a/+page.svelte
@@ -4,4 +4,7 @@
 
 <h1>The page A</h1>
 
-<a href={resolve('/b')}>Go to the page B</a>
+<a href={resolve('/a/b')}>Go to the page B</a>
+<!-- the following line is not working in v1.42 -->
+<!-- i thought it would work with v1.43 but it doesnt either -->
+<!-- <a href={resolve('/b')}>Go to the page B</a> -->
